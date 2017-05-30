c901904f2d3b92ed3b8cbea4f11a10cc686db43b
Antti Leppä <antti.leppa@metatavu.fi>
2017-05-30 15:26:03 +0300

    Travis s3

---
+++ travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm run test-coverage
+TEST_STATUS=$?
+
+if [ "$TEST_STATUS" != "0" ]; then
+  pip install --user awscli
+  export PATH=$PATH:$HOME/.local/bin
+  export S3_PATH=s3://$AWS_BUCKET/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER
+  aws s3 cp target/cargo/configurations/wildfly10x/log $S3_PATH --recursive
+fi
+
+exit $TEST_STATUS
