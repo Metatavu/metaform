350d1124ce17bbbcc2f43086a7da14ce8747d6ff
Antti Leppä <antti.leppa@metatavu.fi>
2017-05-30 15:40:08 +0300

    Fixed s3 upload path on Travis

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ if [ "$TEST_STATUS" != "0" ]; then
   pip install --user awscli
   export PATH=$PATH:$HOME/.local/bin
   export S3_PATH=s3://$AWS_BUCKET/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER
-  aws s3 cp target/cargo/configurations/wildfly10x/log $S3_PATH --recursive
+  aws s3 cp /tmp/metaform/build $S3_PATH --recursive
 fi
 
 exit $TEST_STATUS
